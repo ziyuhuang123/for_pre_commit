7f71680d3f12874dc1cb88499738392f4f468cff
ziyuhuang123 <99854690+ziyuhuang123@users.noreply.github.com>
2023-01-06 18:03:07 +0800

    Add files via upload

---
+++ changed_example.py
@@ -0,0 +1,23 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fileNameList', type=str)
+    args = parser.parse_args()
+    name_list = args.fileNameList.split(":")
+    folder_need_check = set()
+    for loc in name_list:
+        # Find only the sub-folder of 'example' folder
+        if loc.split("/")[0] == "examples" and len(loc.split("/")) >= 4:
+            folder_need_check.add(loc.split("/")[1] + "/" + loc.split("/")[2])
+    # Output the result using print. Then the shell can get the values.
+    folder_need_check = list(folder_need_check)
+    if len(folder_need_check) == 0:
+        print('no_file_to_check')
+    else:
+        print(list(folder_need_check))
+
+
+if __name__ == '__main__':
+    main()
